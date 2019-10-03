103b5e8da86e97bb5fbba97bccffd73b1588af3e
Antonio Matheus <antonimathsilva@gmail.com>
2019-10-03 16:10:38 -0300

    Updates

---
+++ precache-manifest.69c3e8b551335f9d8d89b9dfa4048da3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441649cd563fd076084706041e5404d7",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "9159a660a801dac38abc",
+    "url": "/calculator/static/js/2.09238733.chunk.js"
+  },
+  {
+    "revision": "60a9f85461c0b909072b",
+    "url": "/calculator/static/js/main.2ba67809.chunk.js"
+  },
+  {
+    "revision": "9fb7e923eef29faf5731",
+    "url": "/calculator/static/js/runtime-main.4df7f200.js"
+  }
+]);
